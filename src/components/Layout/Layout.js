1a6ae06df331755551aec84a04ecf0fd985ddbef
Cristian Viterales <cviterales@batan.coop>
2021-02-04 19:44:32 -0300

    first commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Aux from "../../hoc/AuxHoc";
+import {clasess} from './Layout.css'
+
+const layout = (props) => {
+  return (
+    <Aux>
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main className={clasess.content}>{props.children}</main>
+    </Aux>
+  );
+};
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from "../../hoc/AuxHoc";
+
+const BurgerBuilder = () => {
+  return (
+    <Aux>
+      <div>Burger</div>
+      <div>Burger Controls</div>
+    </Aux>
+  );
+};
+
+export default BurgerBuilder;
