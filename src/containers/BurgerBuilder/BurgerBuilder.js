3c4ac756254c803bbcd40dde6027ba9dc7284c5f
Cristian Viterales <cviterales@batan.coop>
2021-02-04 20:51:39 -0300

    added burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Aux from "../../hoc/AuxHoc";
+import Burger from "../../components/Burger/Burger";
 
 const BurgerBuilder = () => {
+  const [ingredient, setIngredient] = useState({
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  });
+
   return (
     <Aux>
-      <div>Burger</div>
+      <Burger ingredients={ingredient}/>
       <div>Burger Controls</div>
     </Aux>
   );
