d35e3f89124a090e23025ed456cfaee882a8b246
Cristian Viterales <cviterales@batan.coop>
2021-02-05 20:28:29 -0300

    added drawer menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Aux from "../../hoc/AuxHoc";
 import clasess from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => {
+  const [showSideDrawer, setShowSideDrawer] = useState(true);
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer((prevState) => {
+      return !prevState
+    });
+  };
+
   return (
     <Aux>
-      <Toolbar />
+      <Toolbar drawerToggleClicked={sideDrawerToggleHandler}/>
+      <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
       <main className={clasess.content}>{props.children}</main>
     </Aux>
   );
