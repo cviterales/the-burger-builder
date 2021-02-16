9243bc7843425f40dfd516e5f8472b934d3766ec
Cristian Viterales <cviterales@batan.coop>
2021-02-15 22:29:52 -0300

    added form contact data for order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,75 +3,197 @@ import React, { useState } from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.css";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 import axios from "../../../axios-order";
 
 const ContactData = (props) => {
-  const state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: "",
+  const [orderForm, setOrderForm] = useState({
+    name: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your Name",
+      },
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
     },
-  };
-  const [loading, setLoading] = useState(false);
+    street: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Street",
+      },
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    zipCode: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "ZIP´Code",
+      },
+      value: "",
+      validation: {
+        required: true,
+        minLength: 5,
+        maxLength: 5,
+        isNumeric: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    country: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Country",
+      },
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "Your E-mail",
+      },
+      value: "",
+      validation: {
+        required: true,
+        isEmail: true,
+      },
+      valid: false,
+      touched: false,
+    },
+    deliveryMethod: {
+      elementType: "select",
+      elementConfig: {
+        options: [
+          { value: "fastest", displayValue: "Fastest" },
+          { value: "cheapest", displayValue: "Cheapest" },
+        ],
+      },
+      value: "fastest",
+      validation: {},
+      valid: true,
+    },
+  });
 
+  const [loading, setLoading] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
   const orderHandler = (e) => {
     e.preventDefault();
     setLoading(true);
+    const formData = {};
+    for (let formElementIdentifier in orderForm) {
+      formData[formElementIdentifier] = orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: props.ingredients,
       price: props.price,
-      customer: {
-        name: "Cristian",
-        address: {
-          street: "TestStreet 1",
-          zipCode: "7601",
-          country: "Argentina",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethod: "fastest",
+      orderData: formData,
     };
     axios
       .post("/orders.json", order)
       .then((response) => {
         setLoading(false);
-        props.history.push('/')
+        props.history.push("/");
       })
       .catch((error) => {
         setLoading(false);
       });
   };
 
+  const checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  };
+  const inputChangedHandler = (e, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...orderForm,
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier],
+    };
+    updatedFormElement.value = e.target.value;
+    updatedFormElement.valid = checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedFormElement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    setFormIsValid(formIsValid);
+    setOrderForm(updatedOrderForm);
+  };
+
+  const formElementArray = [];
+  for (let key in orderForm) {
+    formElementArray.push({
+      id: key,
+      config: orderForm[key],
+    });
+  }
   let form = (
-    <form>
-      <input
-        className={classes.Input}
-        type="text"
-        name="name"
-        placeholder="Your name"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="email"
-        placeholder="Your email"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="street"
-        placeholder="Street"
-      />
-      <input
-        className={classes.Input}
-        type="text"
-        name="postal"
-        placeholder="Postal Code"
-      />
-      <Button btnType="Success" clicked={orderHandler}>
+    <form onSubmit={orderHandler}>
+      {formElementArray.map((formElement) => {
+        return (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
+            changed={(e) => inputChangedHandler(e, formElement.id)}
+          />
+        );
+      })}
+      <Button btnType="Success" disabled={!formIsValid}>
         ORDER
       </Button>
     </form>
